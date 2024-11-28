1c6dbd8492a171fc6eb23c9cae1c6e5b81f9aded
RZJournal <rzjournal@rzjournal.com>
2024-11-28 00:05:56 -0500

    Add Intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,26 @@
 package frc.robot.subsystems;
 
-public class Intake {
-    
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
+
+public class Intake extends SubsystemBase {
+    private final PWMSparkMax intakeMotor;
+
+    public Intake() {
+        intakeMotor = new PWMSparkMax(IntakeConstants.kIntakeMotorPort);
+    }
+
+    public void runIntake() {
+        intakeMotor.set(1.0);
+    }
+
+    public void reverseIntake() {
+        intakeMotor.set(-1.0); 
+    }
+
+    public void stopIntake() {
+        intakeMotor.set(0.0); 
+    }
 }
